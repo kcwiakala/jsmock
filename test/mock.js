e5112940aa309eb003a1f8f305e777682c55a81b
kcwiakala <konrad.cwiakala@gmail.com>
2017-11-24 22:44:17 +0100

    Fixed unexpected call handler

--- test/mock.js
+++ test/mock.js
@@ -15,6 +15,8 @@ describe('mock', () => {
 
     let fooMock = new Mock(foo);
 
+    expect(foo.bar.bind(foo)).to.throw(Error, 'Unexpected');
+
     fooMock.expectCall('bar')
       .matching((a,b) => a > b)
       .will((a, b) => a * b)
@@ -23,11 +25,19 @@ describe('mock', () => {
     expect(foo.bar(6,3)).to.be.equal(18);
     expect(foo.bar(4,2)).to.be.equal(8);
 
-    //fooMock.expectCall('bar').will((a,b) => a - b).times(1);
-    //expect(foo.bar(4,2)).to.be.equal(2);
+    fooMock.expectCall('bar').will((a,b) => a - b).times(1);
+    expect(foo.bar(4,2)).to.be.equal(2);
 
     fooMock.expectCall('bar').will((a,b) => 2*a + b);
     expect(foo.bar(4,2)).to.be.equal(10);
+
+    fooMock.expectCall('bar', 1, 3).willOnce(() => 88);
+    expect(foo.bar(1,3)).to.be.equal(88);
+
+    fooMock.expectCall('bar')
+      .matching(1, 3)
+      .willOnce(() => 32);
+    expect(foo.bar(1,3)).to.be.equal(32);
 
     fooMock.cleanup();
 
